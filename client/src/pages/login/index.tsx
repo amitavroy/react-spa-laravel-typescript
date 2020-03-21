017075ce8df42223714a61039991f77ddb5f0a7d
Amitav Roy <reachme@amitavroy.com>
2020-03-21 20:55:06 +0530

    Worked on the login component

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
+import './_style.scss';
 
 class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    isChecked: false
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.username, this.state.password);
+  }
+
+  handleChecked() {
+    this.setState({ isChecked: !this.state.isChecked });
+  }
+
   render() {
+    const { username, password, isChecked } = this.state;
     return (
       <React.Fragment>
         <div className="login-page">
           <div className="login-box">
             <div className="login-logo">
-              <a href="/" onClick={(event) => { event.preventDefault() }}><b>Admin</b>LTE</a>
+              <a href="/" onClick={(event) => { event.preventDefault() }}><b>React</b>ADMIN</a>
             </div>
 
             <div className="card">
               <div className="card-body login-card-body">
                 <p className="login-box-msg">Sign in to start your session</p>
 
-                <form action="../../index3.html" method="post">
+                <form onSubmit={(event) => this.handleFormSubmit(event)}>
                   <div className="input-group mb-3">
-                    <input type="email" className="form-control" placeholder="Email" />
+                    <input type="email"
+                      name="name"
+                      className="form-control"
+                      placeholder="Email"
+                      value={username}
+                      onChange={event => this.setState({ username: event.target.value })} />
                     <div className="input-group-append">
                       <div className="input-group-text">
                         <span className="fas fa-envelope"></span>
@@ -24,7 +46,12 @@ class Login extends Component {
                     </div>
                   </div>
                   <div className="input-group mb-3">
-                    <input type="password" className="form-control" placeholder="Password" />
+                    <input type="password"
+                      name="password"
+                      className="form-control"
+                      placeholder="Password"
+                      value={password}
+                      onChange={event => this.setState({ password: event.target.value })} />
                     <div className="input-group-append">
                       <div className="input-group-text">
                         <span className="fas fa-lock"></span>
@@ -34,8 +61,8 @@ class Login extends Component {
                   <div className="row">
                     <div className="col-8">
                       <div className="icheck-primary">
-                        <input type="checkbox" id="remember" />
-                        <label>Remember Me</label>
+                        <input type="checkbox" id="remember" onChange={() => this.handleChecked()} checked={isChecked} />
+                        <label onClick={() => this.handleChecked()} id="remember-label">Remember Me</label>
                       </div>
                     </div>
                     <div className="col-4">
