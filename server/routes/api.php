7bea47154003b4ee46d2b13d34e880b4eeaae51e
Amitav Roy <reachme@amitavroy.com>
2020-04-10 12:27:35 +0530

    Adding the support of Git hooks for code formatting

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use Laravel\Passport\Http\Controllers\AccessTokenController;
 
 Route::post('login', [AccessTokenController::class, 'issueToken'])->middleware(['api-login', 'throttle']);
@@ -11,7 +11,7 @@ Route::middleware(['auth:api', 'get.expiry'])->get('/user', function (Request $r
     return $request->user();
 });
 
-Route::group(['middleware' => ['auth:api']], function() {
+Route::group(['middleware' => ['auth:api']], function () {
     Route::post('/user', [UserController::class, 'store']);
 });
 
