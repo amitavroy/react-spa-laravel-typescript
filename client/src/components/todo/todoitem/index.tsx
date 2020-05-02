dcf0216cbb1d279bce2a1ed36743a5232a92dce2
Amitav Roy <reachme@amitavroy.com>
2020-05-02 12:23:17 +0530

    before starting working on the modal

--- client/src/components/todo/todoitem/index.tsx
+++ client/src/components/todo/todoitem/index.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import TodoInterface from './../../../interfaces/TodoInterface';
+import TodoInterface from "./../../../interfaces/TodoInterface";
 
 interface Props {
-  todo: TodoInterface
-  markTodoCompelete: any
-  dragHandle: any
+  todo: TodoInterface;
+  markTodoCompelete: any;
+  dragHandle: any;
 }
 
 class TodoItem extends Component<Props> {
@@ -17,16 +17,25 @@ class TodoItem extends Component<Props> {
   render() {
     const { todo, dragHandle } = this.props;
     return (
-      <li className={`${(todo.is_completed == true) ? 'done' : ''}`}>
+      <li className={`${todo.is_completed === true ? "done" : ""}`}>
         {dragHandle}
         <div className="icheck-primary d-inline ml-2">
-          <input type="checkbox" value=""
-            onChange={event => this.handleTodoCheck(event)}
-            name={`todo${todo.id}`} id={`todoCheck${todo.id}`}
-            checked={(todo.is_completed == true) ? true : false} />
+          <input
+            type="checkbox"
+            value=""
+            onChange={(event) => this.handleTodoCheck(event)}
+            name={`todo${todo.id}`}
+            id={`todoCheck${todo.id}`}
+            checked={todo.is_completed == true ? true : false}
+          />
           <label htmlFor={`todoCheck${todo.id}`}></label>
         </div>
-        <span className="text cursor" onClick={event => this.handleTodoCheck(event)}>{todo.description}</span>
+        <span
+          className="text cursor"
+          onClick={(event) => this.handleTodoCheck(event)}
+        >
+          {todo.description}
+        </span>
         <small className={`badge ${todo.badge}`}>
           <i className="far fa-clock"></i> {todo.pending}
         </small>
