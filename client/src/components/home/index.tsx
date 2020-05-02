b283d06b7b8c0fc89237116fb3d77ba0f74a34df
Amitav Roy <reachme@amitavroy.com>
2020-05-02 22:32:39 +0530

    11 - Creating modal pop up to show the Add Todo form

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -2,17 +2,23 @@ import React, { Component } from "react";
 import Card from "./../common/card";
 import TopCards from "./topcards";
 import TodoList from "./../todo/todolist";
+import Modal from "../common/modal";
 
-const TodoFooter = () => {
+const TodoFooter = (props) => {
   return (
-    <span>
-      This is the footer with <strong>bold</strong> & <i>italics</i>
-    </span>
+    <button
+      className="btn btn-primary btn-sm"
+      onClick={() => props.handleAddTodo()}
+    >
+      Add new Todo
+    </button>
   );
 };
 
 class Home extends Component {
+  state = { showModal: false };
   render() {
+    const { showModal } = this.state;
     return (
       <React.Fragment>
         <TopCards />
@@ -21,9 +27,24 @@ class Home extends Component {
             <Card
               title="Todo list"
               titleIcon="ion-clipboard"
-              footer={<TodoFooter />}
+              footer={
+                <TodoFooter
+                  showModal={showModal}
+                  handleAddTodo={() => this.setState({ showModal: true })}
+                />
+              }
             >
               <TodoList />
+              <Modal
+                title="Add new Todo"
+                visibility={showModal}
+                size="lg"
+                onClose={() => this.setState({ showModal: !showModal })}
+              >
+                <p>
+                  This is where the form will <strong>come</strong>.
+                </p>
+              </Modal>
             </Card>
           </div>
           <div className="col-md-5">
