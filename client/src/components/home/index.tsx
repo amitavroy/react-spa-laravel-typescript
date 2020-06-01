5b769adea40e0a85e0bd223008c5233847935b79
Amitav Roy <reachme@amitavroy.com>
2020-06-01 16:21:38 +0530

    Refactored the code to have a Todo wrapper component which can be used in place page and it will just work.

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -1,79 +1,21 @@
 import React, { Component } from "react";
 import Card from "./../common/card";
 import TopCards from "./topcards";
-import TodoList from "./../todo/todolist";
-import Modal from "../common/modal";
-import TodoAdd from "../todo/todoadd";
-import TodoService from "../../services/TodoService";
-import TodoEvent from "../../interfaces/TodoEvent";
-
-const TodoFooter = (props) => {
-  return (
-    <button
-      className="btn btn-primary btn-sm"
-      onClick={() => props.handleAddTodo()}
-    >
-      Add new Todo
-    </button>
-  );
-};
+import TodoWrapper from "../todo/todowrapper";
 
 class Home extends Component {
-  state = { showModal: false, todos: [] };
-  async componentDidMount() {
-    const response = await TodoService.getTodoList();
-    this.setState({ todos: response });
-    this.handleTodoDelete();
-  }
-  handleTodoDelete() {
-    TodoService.getObservable().subscribe(async (event: TodoEvent) => {
-      const postData = { id: event.data.id };
-      await TodoService.removeTodo(postData);
-      let newTodos = this.state.todos.filter((todo) => {
-        return event.data.id !== todo.id;
-      });
-      this.setState({ todos: newTodos });
-    });
-  }
-  componentWillUnmount() {
-    TodoService.getObservable().unsubscribe();
-  }
-  async handleTodoAdd(data) {
-    const todo = await TodoService.saveNewTodo(data);
-    let newTodos = this.state.todos;
-    newTodos.unshift(todo);
-    this.setState({ todo: newTodos });
-  }
   render() {
-    const { showModal, todos } = this.state;
     return (
       <React.Fragment>
         <TopCards />
         <div className="row">
-          <div className="col-md-7">
-            <Card
-              title="Todo list"
-              titleIcon="ion-clipboard"
-              footer={
-                <TodoFooter
-                  showModal={showModal}
-                  handleAddTodo={() => this.setState({ showModal: true })}
-                />
-              }
-            >
-              {todos.length > 0 && <TodoList todosProp={todos} />}
-              <Modal
-                title="Add new Todo"
-                visibility={showModal}
-                size="lg"
-                onClose={() => this.setState({ showModal: !showModal })}
-              >
-                <TodoAdd onTodoAdd={(data) => this.handleTodoAdd(data)} />
-              </Modal>
-            </Card>
+          <div className="col-md-6">
+            <TodoWrapper />
           </div>
           <div className="col-md-5">
-            <Card title="Demo list">Some content will come here later.</Card>
+            <Card title="Some content will come" titleIcon="ion-clipboard">
+              <p>Content will come.</p>
+            </Card>
           </div>
         </div>
       </React.Fragment>
--- client/src/services/UrlService.ts
+++ client/src/services/UrlService.ts
@@ -2,7 +2,7 @@ let apiDomain = "";
 if (process.env.NODE_ENV === "production") {
   apiDomain = "https://life-api.amitavroy.com/";
 } else {
-  apiDomain = "http://localhost:8000/";
+  apiDomain = "http://localhost:8001/";
 }
 
 class UrlService {
